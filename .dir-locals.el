73039def6d99727869ce9a89fb50761aa6f908cc
Rodney Price <price-rodney@zai.com>
2023-12-21 18:13:21 -0700

    Run down errors due to Windows

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((project-vc-name . "torchsig")))
+ (python-mode . ((conda-project-env-path . "torchsig"))))
